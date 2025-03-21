be2e8106b83ffaa46dcd9275e66d37e744c425a2
Akame style <eiljo@mail.ru>
2025-03-21 03:21:44 +0300

    Initial commit

---
+++ src/Box.java
@@ -0,0 +1,29 @@
+public class Box<A, B, C>
+{
+    private A item1;
+    private B item2;
+    private C item3;
+
+    public Box(A item1, B item2, C item3) {
+        this.item1 = item1;
+        this.item2 = item2;
+        this.item3 = item3;
+    }
+
+    int sumOfNums()
+    {
+        int sum = 0;
+
+        if (item1 instanceof Number) {
+            sum += ((Number) item1).floatValue();
+        }
+        if (item2 instanceof Number) {
+            sum += ((Number) item2).floatValue();
+        }
+        if (item3 instanceof Number) {
+            sum += ((Number) item3).floatValue();
+        }
+        return sum;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Box box1 = new Box(1, 1, "f");
+        Box box2 = new Box(3, 0, "f");
+        Box box3 = new Box("КАКОЙЖЕЯКРАСИВЫЙ", 5, "f");
+        System.out.println(box1.sumOfNums() + box2.sumOfNums() + box3.sumOfNums());
+    }
+}
